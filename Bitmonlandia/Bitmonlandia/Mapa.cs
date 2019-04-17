87e4eb4eb9c2cfbd48c7646a1dc5371f6606617f
Track-tor <48454411+Track-tor@users.noreply.github.com>
2019-04-17 19:25:27 -0400

    Add files via upload

--- Bitmonlandia/Bitmonlandia/Mapa.cs
+++ Bitmonlandia/Bitmonlandia/Mapa.cs
@@ -8,5 +8,22 @@ namespace Bitmonlandia
 {
     class Mapa
     {
+        private int alto;
+        private int ancho;
+        private string[,,] tablero;
+
+        public Mapa(int alto, int ancho)
+        {
+            this.alto = alto;
+            this.ancho = ancho;
+            tablero = new string[ancho, alto, 2];
+        }
+
+        public string[,,] GetTablero()
+        {
+            return tablero;
+
+        }
+
     }
 }
