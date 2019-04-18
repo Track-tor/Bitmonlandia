db5743aa3e4675f59c9d6d95b7d24ac2a9fdcabe
mp11b <48453189+mp11b@users.noreply.github.com>
2019-04-18 17:07:56 -0400

    Add files via upload

--- Bitmonlandia/Bitmonlandia/Program.cs
+++ Bitmonlandia/Bitmonlandia/Program.cs
@@ -11,33 +11,20 @@ namespace Bitmonlandia
         static void Main(string[] args)
         {
             Bitmonlandia bitmonlandia = new Bitmonlandia();
-            int[] tupla = {1,2};
-            bitmonlandia.lista_bitmons_totales.Add(new Ent(1, 2, 3,tupla));
-            bitmonlandia.
-            Ent ent1 = new Ent();
-            Ent ent2 = new Ent();
-            Taplan taplan1 = new Taplan();
-            Taplan taplan2 = new Taplan();
-            Doti doti1 = new Doti();
-            Doti doti2 = new Doti();
-            Dorvalo dorvalo1 = new Dorvalo();
-            Dorvalo dorvalo2 = new Dorvalo();
-            Gofue gofue1 = new Gofue();
-            Gofue gofue2 = new Gofue();
-            Wetar wetar1 = new Wetar();
-            Wetar wetar2 = new Wetar();
-            lista_bitmon_totales.Add(ent1);
-            lista_bitmon_totales.Add(ent2);
-            lista_bitmon_totales.Add(taplan1);
-            lista_bitmon_totales.Add(taplan2);
-            lista_bitmon_totales.Add(doti1);
-            lista_bitmon_totales.Add(doti2);
-            lista_bitmon_totales.Add(dorvalo1);
-            lista_bitmon_totales.Add(dorvalo2);
-            lista_bitmon_totales.Add(gofue1);
-            lista_bitmon_totales.Add(gofue2);
-            lista_bitmon_totales.Add(wetar1);
-            lista_bitmon_totales.Add(wetar2);
+            int[] tupla = { 1, 2 };
+
+            bitmonlandia.lista_bitmons_totales.Add(new Ent("Ent",1, 2, 3,tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Ent("Ent",1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Taplan("Taplan",1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Taplan("Taplan", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Doti("Doti", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Doti("Doti", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Dorvalo("Dorvalo", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Dorvalo("Dorvalo", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Wetar("Wetar", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Wetar("Wetar", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Gofue("Gofue", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Gofue("Gofue", 1, 2, 3, tupla));
 
         }
     }
