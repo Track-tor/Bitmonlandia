4ae6193b48c5444891f634ff3ac5278ee8ecb529
mp11b <48453189+mp11b@users.noreply.github.com>
2019-04-18 17:07:20 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitmonlandia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmonlandia bitmonlandia = new Bitmonlandia();
+            int[] tupla = { 1, 2 };
+
+            bitmonlandia.lista_bitmons_totales.Add(new Ent("Ent",1, 2, 3,tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Ent("Ent",1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Taplan("Taplan",1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Taplan("Taplan", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Doti("Doti", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Doti("Doti", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Dorvalo("Dorvalo", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Dorvalo("Dorvalo", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Wetar("Wetar", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Wetar("Wetar", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Gofue("Gofue", 1, 2, 3, tupla));
+            bitmonlandia.lista_bitmons_totales.Add(new Gofue("Gofue", 1, 2, 3, tupla));
+
+        }
+    }
+}
